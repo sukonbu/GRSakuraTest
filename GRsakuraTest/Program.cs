c756de698abd71434989b1de4973406839660883
sukonbu <t.cfhhy.skn0909@gmail.com>
2015-10-19 17:52:11 +0900

    inputPort処理とInterrupt処理

--- GRsakuraTest/Program.cs
+++ GRsakuraTest/Program.cs
@@ -6,10 +6,14 @@ namespace GRsakuraTest
 {
     public class Program
     {
+        private const Cpu.Pin ButtonPin = (Cpu.Pin)0x57;
+        private const Cpu.Pin UserPin = (Cpu.Pin)0x56;
+
+        private static OutputPort _led;
+
+
         public static void Main()
         {
-            const int SleepTime = 500; //const ‚ÍPascal
-            
             //GR-SAKURA on board LED pin (LED1 0x50, LED2 0x51, LED3 0x52, LED4 0x56)
             Cpu.Pin[] ledPins = { (Cpu.Pin)0x50, (Cpu.Pin)0x51, (Cpu.Pin)0x52, (Cpu.Pin)0x56 };
 
@@ -17,12 +21,29 @@ namespace GRsakuraTest
                                   new OutputPort(ledPins[1], false),
                                   new OutputPort(ledPins[2], false),
                                   new OutputPort(ledPins[3], false) };
+            var button = new InterruptPort(ButtonPin, true, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeBoth);
+            button.OnInterrupt += button_OnInterruput;
+            _led = new OutputPort(UserPin, false);
+            (new Thread(() =>
+            {
+                Thread.Sleep(Timeout.Infinite);
+            })).Start();
+        }
 
+        static void button_OnInterruput(uint data1,uint data2,DateTime time)
+        {
+            var isPressed = data2 == 0;
+            _led.Write(isPressed);
+        }
+
+        static void LedBlink(OutputPort[] leds)
+        {
+            int SleepTime = 500; //const ‚ÍPascal
             (new Thread(() =>
             {
                 while (true)
                 {
-                    foreach(OutputPort led in leds)
+                    foreach (OutputPort led in leds)
                     {
                         led.Write(false);
                     }
@@ -33,6 +54,22 @@ namespace GRsakuraTest
                         led.Write(true);
                     }
                     Thread.Sleep(SleepTime);
+                }
+            })).Start();
+        }
+
+        static void inputButton(OutputPort[] leds)
+        {
+            var button = new InputPort(ButtonPin, true, Port.ResistorMode.Disabled);
+            var led = new OutputPort(UserPin, false);
+            (new Thread(() =>
+            {
+                while (true)
+                {
+                    var isPressed = !button.Read();
+
+                    led.Write(isPressed);
+                    Thread.Sleep(100);
                 }
             })).Start();
         }
